3c7d2e0182e309c120df7c25b8184b50cbd7e50a
Ynonne-Martin <yvonnemartinkatumbi@gmail.com>
2024-09-19 19:52:09 +0300

    made changes

--- landvista/location/models.py
+++ landvista/location/models.py
@@ -17,21 +17,7 @@ class Location(models.Model):
    def __str__(self):
        return self.name
 
-# from django.db import models
 
-
-# # # Create your models here.
-
-
-# # class Location(models.Model):
-# #    name = models.CharField(max_length=255)
-# #    latitude = models.DecimalField(max_digits=10, decimal_places=8)
-# #    longitude = models.DecimalField(max_digits=11, decimal_places=8)
-# #    searched_at = models.DateTimeField(auto_now_add=True)
-
-
-# #    def __str__(self):
-# #        return self.name
   
 
 
