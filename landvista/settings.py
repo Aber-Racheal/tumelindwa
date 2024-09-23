7cab49df776fc4e9b50c155de75ccc8215597fbe
Mary Muthoni <muthonimaryvivian@gmail.com>
2024-09-23 10:22:06 +0300

    made changes

--- landvista/settings.py
+++ landvista/settings.py
@@ -115,9 +115,9 @@ REDIRECT_URI = os.environ.get("REDIRECT_URI", '')
 GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY", '')
 
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND",'django.core.mail.backends.smtp.EmailBackend')
-EMAIL_HOST=os.getenv('EMAIL_HOST','smtp.gmail.com')
+EMAIL_HOST=os.getenv('EMAIL_HOST','smtp.gmail.com',)
 EMAIL_PORT=int(os.getenv('EMAIL_PORT',587))
 EMAIL_USE_TLS=os.getenv('EMAIL_USE_TLS','True')=='True'
-EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD', '')
 
