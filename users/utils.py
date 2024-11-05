62f442e5cd07da57200d259abb761263bfa4036f
Mary Muthoni <muthonimaryvivian@gmail.com>
2024-11-05 10:52:27 +0300

    added base url to the env file

--- users/utils.py
+++ users/utils.py
@@ -36,7 +36,7 @@ def send_email(subject, to_email, context, template_name='email_templates.html')
 def generate_confirmation_link(user, request):
     token = default_token_generator.make_token(user)
     uid = urlsafe_base64_encode(force_bytes(user.pk))
-    base_url = 'https://landvista-dashboard.vercel.app/dashboard'  # Correct base URL
+    base_url = settings.BASE_URL  
     confirmation_url = f"{base_url}/confirm-registration/{uid}/{token}/"
     return confirmation_url
 
